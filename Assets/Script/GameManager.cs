c344a3ebc4cc01117d1b4f85c06c65ada242650c
gpos-kim <dmsghs@postech.ac.kr>
2018-02-06 19:05:28 +0900

    RogueLikeLikeGame
    It is started already!

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private int currentTurn=0;
+    private bool currentSituation=false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+    
+    void AttackToEnemy(Enemy enemy) {
+
+    }
+    
+    void AttackToPlayer(Enemy enemy) {
+
+    }
+
+    void GenerateMonsters(int numberOfMonster) {
+
+    }
+}
