05e6630c36e0039ef130a224364e4338d6bec083
SungBinPark <psb2942@gmail.com>
2018-02-07 15:33:39 +0900

    Item, ItemManager Code
    I wrote Item and ItemManager Code.

---
+++ Assets/Script/Item.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+    private string property;
+
+    public string Property
+    {
+        get
+        {
+            return property;
+        }
+
+        protected set
+        {
+            property = value;
+        }
+    }
+
+    private new string name;
+
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+
+        protected set
+        {
+            name = value;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
