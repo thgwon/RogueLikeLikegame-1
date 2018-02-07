8ac0bb0c6b004de24cd42ce8df6696d7680c3de9
thgwon <thgwon@postech.ac.kr>
2018-02-07 11:38:40 +0900

    Player, Status, Unit base script
    These are base script of Player, Status, and Unit.

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Unit {
+    private int mp;
+    private int hungry;
+    private const int invenSize = 12;
+    private int [] itemList = new itemList [invenSize];
+	
+}
---
+++ Assets/Script/Unit.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Unit : MonoBehaviour {
+    private int attack;
+    private int defense;
+    private int hp;
+    private List<Status> statusList;
+
+    public int Attack
+    {
+        get
+        {
+            return attack;
+        }
+
+        set
+        {
+            attack = value;
+        }
+    }
+    public int Defense
+    {
+        get
+        {
+            return defense;
+        }
+
+        set
+        {
+            defense = value;
+        }
+    }
+    public int Hp
+    {
+        get
+        {
+            return hp;
+        }
+
+        set
+        {
+            hp = value;
+        }
+    }
+    ;
+
+    public abstract void changeAttack(int delta);
+    public abstract void changeHp(int delta);
+    public abstract void changeDefense(int delta);
+}
+
+
+
